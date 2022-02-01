7e3f9a46ae68bc96f9ea76c0a5275bd79fcdd020
Eggys321 <imonieroheguono@gmail.com>
2022-02-01 16:08:17 +0100

    fuit commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([])
+  console.log(data);
+  useEffect( ()=>{
+    getData()
+    
+
+  },[] )
+
+  const getData = async()=>{
+    const response = await fetch('https://fakestoreapi.com/products')
+    // console.log(response)
+    const jsonData = await response.json()
+    // console.log(jsonData);
+    setData(jsonData)
+
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Fetch Api</h1>
+      <div >
+        {data.map((values)=>{
+          return (
+            <div className='text'>
+              <h1> {values.title} </h1>
+              <h1> {values.description} </h1>
+              <img src={values.image} alt="" />
+            </div>
+          )
+
+        } )}
+        {/* <h4>Title</h4> */}
+        {/* <h4>Title</h4>
+        <h4>Title</h4>
+        <h4>Title</h4>
+        <h4>Title</h4> comment 4 ..once i fetch data,d data wud be created in dt fashion */}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
